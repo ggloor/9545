0dbdbeb9494bfa0e2124f5722048a96ec40b62bc
ggloor <ggloor@uwo.ca>
2016-11-28 16:36:45 -0500

    exploring an RNA-seq dataset

---
+++ R_section/explore_barton.R
@@ -0,0 +1,15 @@
+---
+title: "explore outliers"
+author: "gg"
+date: "Nov 28, 2016"
+output: pdf_document
+---
+
+```{r lib, include=FALSE, message=FALSE, warning=FALSE, echo=TRUE}
+
+library(zCompositions)
+library(ALDEx2)
+library(CoDaSeq)
+
+plot=FALSE
+```
